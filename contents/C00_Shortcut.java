860ad7e2c700712282b9817c8dede1dfff573b00
ParkGiHyun <einght4046@naver.com>
2023-03-18 19:09:59 +0900

    about shortcut in eclipse & window

---
+++ contents/C00_Shortcut.java
@@ -0,0 +1,33 @@
+/*
+# 이클립스 단축키 
+1. 프로젝트창
+현재페이지 전체화면: ctrl + M
+화면 세로 분할: ctrl + shift + {
+화면 가로 분할: ctrl + shift + _ 
+
+2. 코드 작성시
+한줄(영역) 이동: alt + ↓,↑
+한줄(영역) 복사: alt + ctrl + ↓,↑
+한줄(영역) 삭제: ctrl + D
+코드 자동 정렬: ctrl + shift + F
+
+# 윈도우 공통 단축키
+파일명 수정: F2
+다시실행: ctrl + Z
+선택영역 잘라내기/복사/붙여넣기: ctrl + X/C/V
+전체 선택: ctrl + A
+단어 찾기: ctrl + F
+현재창 닫기: ctrl + W
+
+한줄 맨앞뒤 이동: home, end
+한줄 영역지정: shift + home, end
+단어 단위 이동: ctrl + ←,→
+단어 단위 영역지정: shift + ctrl + ←,→
+단어 단위 삭제: ctrl + delete/backspace
+		
+*/
+package contents;
+
+public class C00_Shortcut {
+
+}
